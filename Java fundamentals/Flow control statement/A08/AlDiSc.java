b9befbd482fec73a8771079cc80d8430bc6c5553
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-15 22:39:28 +0530

    Add files via upload

---
+++ Java fundamentals/Flow control statement/A08/AlDiSc.java
@@ -0,0 +1,22 @@
+public class AlDiSc
+{
+public static void main(String[] args)
+{
+      Scanner scanner=new Scanner(System.in);
+      String char1 = String.valueOf(scanner.next().charAt(0));
+
+      if(char1.matches("[0-9]") || char1.matches("-[0-9]"))
+      {
+        System.out.print("char is Digit");
+
+      }
+      else if(char1.matches("[a-zA-Z]"))
+      {
+        System.out.print("char is Alphabet");
+      }
+      else
+      {
+        System.out.print("char is special character");
+          }
+      }
+    }
