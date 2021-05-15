ed09197988ee5de8ae2837e1e465102ecd13ec9d
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-15 23:26:28 +0530

    Add files via upload

---
+++ Java fundamentals/Arrays/A25/ArrayPresent.java
@@ -0,0 +1,20 @@
+public class ArrayPresent{
+  public static void main(String[] args) {
+
+    int[] num = {1, 4, 34, 56, 7};
+    int toFind = 5;
+    boolean found = false;
+
+    for (int n : num) {
+      if (n == toFind) {
+        found = true;
+        break;
+      }
+    }
+    
+    if(found)
+      System.out.println(toFind + " is found.");
+    else
+      System.out.println(toFind + " -1");
+  }
+}
