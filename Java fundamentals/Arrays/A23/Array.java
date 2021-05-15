0b42822ce8cffe0891cc51ec657c4f6e8455ca72
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-15 23:24:23 +0530

    Add files via upload

---
+++ Java fundamentals/Arrays/A23/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+ public class Array 
+ { 
+               public static void main(String args[]) 
+         { 
+               int sum=0; 
+               Scanner s=new Scanner(System.in); 
+               System.out.print("Enter The Size of the Array : "); 
+               int size=s.nextInt(); 
+               int arr[]=new int[size]; 
+               System.out.println("Enter the elements of Array"); 
+               for(int i=0;i<arr.length;i++) 
+                  { 
+                     arr[i]=s.nextInt(); 
+                     sum+=arr[i]; 
+                   } 
+                     float avg=(float)(sum/arr.length); 
+             System.out.println("Sum is "+sum+" Average is "+avg); 
+         } 
+ } 
+ 
