f229956e6abbc3a7f91e1b617ab7b5f700de1787
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-15 22:54:17 +0530

    Add files via upload

---
+++ Java fundamentals/Flow control statement/A14/even.java
@@ -0,0 +1,12 @@
+public class Even {
+   public static void main(String args[]) {
+	int n = 57;
+	System.out.print("Even Numbers from 23 to "+n+" are: ");
+	for (int i = 23; i <= n; i++) {
+	   
+	   if (i % 2 == 0) {
+		System.out.print(i + " ");
+	   }
+	}
+   }
+}
