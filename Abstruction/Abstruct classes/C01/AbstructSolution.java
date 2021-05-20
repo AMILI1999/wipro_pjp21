929ebcd7f7a1b74c953bc147cb9a29f4a0edf587
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-20 20:33:24 +0530

    Add files via upload

---
+++ Abstruction/Abstruct classes/C01/AbstructSolution.java
@@ -0,0 +1,62 @@
+abstract class GeneralBank {
+	abstract double getSavingsInterestRate();
+	abstract double getFixedDepositInterestRate();
+}
+
+class ICICIBank extends GeneralBank {
+
+	
+	double getSavingsInterestRate() {
+		
+		return 4;
+	}
+
+	
+	double getFixedDepositInterestRate() {
+		
+		return 8.5;
+	}
+	
+}
+
+class KotMBank extends GeneralBank {
+
+	
+	double getSavingsInterestRate() {
+		
+		return 6;
+	}
+
+	
+	double getFixedDepositInterestRate() {
+		
+		return 9;
+	}
+	
+}
+
+public class AbstructSolution {
+
+	public static void main(String[] args) {
+		
+		ICICIBank iciciBank = new ICICIBank();
+		KotMBank kotMBank = new KotMBank();
+		System.out.println("ICICI BANK: " + 
+				"Fixed Rate = " + iciciBank.getFixedDepositInterestRate() + "%, " + 
+				"Saving Rate = " + iciciBank.getSavingsInterestRate() + "%");
+		System.out.println("KOTAK MAHINDRA BANK: " + 
+				"Fixed Rate = " + kotMBank.getFixedDepositInterestRate() + "%, " + 
+				"Saving Rate = " + kotMBank.getSavingsInterestRate() + "%");
+		System.out.println("----------------------------------------------------------");
+		GeneralBank gBank1 = new ICICIBank();
+		GeneralBank gBank2 = new KotMBank();
+		System.out.println("GENERAL BANK1: " + 
+				"Fixed Rate = " + gBank1.getFixedDepositInterestRate() + "%, " + 
+				"Saving Rate = " + gBank1.getSavingsInterestRate() + "%");
+
+		System.out.println("GENERAL BANK2: " + 
+                "Fixed Rate = " + gBank2.getFixedDepositInterestRate() + "%, " + 
+                "Saving Rate = " + gBank2.getSavingsInterestRate() + "%");	
+	}
+
+}
