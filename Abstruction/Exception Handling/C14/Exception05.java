9d2e0e7bc49ee6aa3b84c4f76890f8d6ef18a51d
palamili <69345542+AMILI1999@users.noreply.github.com>
2021-05-20 21:17:32 +0530

    Add files via upload

---
+++ Abstruction/Exception Handling/C14/Exception05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Exception05 {
+public static void main(String[] args) throws ArithmeticException {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter 1st integer number: ");
+		int num1 = scan.nextInt();
+		System.out.print("Enter 2nd integer number: ");
+		int result = 0;
+		int num2 = scan.nextInt();
+		try {
+			result = num1/num2;
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("Division is " + result);
+	}
+
+}
